c116096efaf478e330f08929b2c507d2dd60c6a1
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-06-29 17:05:14 -0500

    Added findByValue demo example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,18 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner in = new Scanner(System.in);
+    public static ArrayList<HashMap<String, String>> studentList = new ArrayList<>();
 
     public static void main(String[] args) {
 
         // let's create 4 different hashmaps that contain information about students:
 
-        // studentOne (firstName=Bob, lastName=Drummond, course=Spanish101
+        // studentOne (firstName=Bob, lastName=Drummond, course=Spanish101)
         HashMap<String, String> studentOne = new HashMap<>();
         studentOne.put("firstName", "Bob");
         studentOne.put("lastName", "Drummond");
@@ -20,7 +24,7 @@ public class Main {
 //        System.out.println(studentOne.get("lastName")); // what does this print?
 //        System.out.println(studentOne.get("course")); // what does this print?
 
-        // studentTwo (firstName=Tina, lastName=Turner, course=English101
+        // studentTwo (firstName=Tina, lastName=Turner, course=English101)
         HashMap<String, String> studentTwo = new HashMap<>();
         studentTwo.put("firstName", "Tina");
         studentTwo.put("lastName", "Turner");
@@ -29,7 +33,7 @@ public class Main {
         // studentThree (firstName=Fred, lastName=Flinstone, course=English101)
         HashMap<String, String> studentThree = new HashMap<>();
         studentThree.put("firstName", "Fred");
-        studentThree.put("lastName", "Flinstone");
+        studentThree.put("lastName", "Flintstone");
         studentThree.put("course", "English101");
 
         // studentFour (firstName=Bob, lastname=Hope, course=Spanish101
@@ -38,56 +42,43 @@ public class Main {
         studentFour.put("lastName", "Hope");
         studentFour.put("course", "Spanish101");
 
-        // now let's pull all four of our student hashmaps into an array list:
-        ArrayList<HashMap<String, String>> studentList = new ArrayList<>();
+        // now let's pull all four of our student hashmaps into the static array list:
         studentList.add(studentOne);
         studentList.add(studentTwo);
         studentList.add(studentThree);
         studentList.add(studentFour);
 
-        // check your expectations:
-//        System.out.println(studentList.get(0).get("firstName")); // what does this print?
-//        System.out.println(studentList.get(1).get("firstName")); // what does this print?
-//        System.out.println(studentList.get(2).get("firstName")); // what does this print?
+        System.out.println(studentOne.keySet());
+        System.out.println(studentOne.values());
+        // prompt for input
+        System.out.println("Enter a search term:");
+        String userInput = in.nextLine();
 
-        // further expectation checking:
-//        System.out.println(studentList.get(3).get("course")); // what does this print?
+        ArrayList<HashMap<String, String>> searchedStudents = searchUsingInput(userInput);
+        // loop through ArrayList and then loop through each HashMap to print out each key value pair
+        for(HashMap<String, String> currentStudent : searchedStudents) {
+            System.out.println("-------");
+            for(String key : currentStudent.keySet()) {
+                System.out.println(key + ": " + currentStudent.get(key));
+            }
+            System.out.println("-------");
+        }
+    }
 
-        // now let's loop through all the students in our ArrayList printing their first names:
-//        for(int i = 0; i < studentList.size(); i++) {
-//            HashMap<String, String> currentStudent = studentList.get(i);
-//            System.out.println(currentStudent.get("firstName"));
-//        }
+    public static ArrayList<HashMap<String, String>> searchUsingInput(String searchInput) {
 
-        // alternate syntax using an enhanced for loop:
-//        for(HashMap<String, String> currentStudent : studentList) {
-//            System.out.println(currentStudent.get("firstName");
-//        }
+        ArrayList<HashMap<String, String>> students = new ArrayList<>();
 
-        // now let's loop through all the students in our ArrayList printing their course:
-//        for(HashMap<String, String> currentStudent : studentList) {
-//            System.out.println(currentStudent.get("course"));
-//        }
+        for(HashMap<String, String> student : studentList) {
+            for(String value : student.values()) {
+                if(value.contains(searchInput)) {
+                    students.add(student);
+                    // searchInput matching 1 value is sufficient to add in student to students
+                    break;
+                }
+            }
+        }
 
-        // now let's loop through all the students in our ArrayList and loop through all their keys associated with that HashMap
-//        for(HashMap<String, String> currentStudent : studentList) {
-//            for(String key : currentStudent.keySet()) {
-//                System.out.println(key);
-//            }
-//        }
-
-        // now let's loop through allt he students in our ArrayList and loop through all the values associated with the key in that HashMap
-//        for(HashMap<String, String> currentStudent : studentList) {
-//            for(String key : currentStudent.keySet()) {
-//                System.out.println(currentStudent.get(key));
-//            }
-//        }
-
-        // now let's loop through all the students in our ArrayList printing out their key value pairs
-//        for(HashMap<String, String> currentStudent : studentList) {
-//            for(String key : currentStudent.keySet()) {
-//                System.out.println(key + ": " + currentStudent.get(key));
-//            }
-//        }
+        return students;
     }
 }
